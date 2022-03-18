1205cbb6a23005f98d54740e54634eaa4719d4a2
Freyder Montano Perlaza <FreymanMP@gmail.com>
2022-03-17 23:46:31 -0300

    creando section video e imagenes

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,79 @@ moreOptions.addEventListener("click", (e) => {
   e.preventDefault();
   moreMenu.classList.toggle("show");
 });
+
+//mostrar video
+const videos = [
+  {
+    id: "PyMlV5_HRWk",
+  },
+  {
+    id: "XCbMVbeKlCg",
+  },
+  {
+    id: "Fmdb-KmlzD8",
+  },
+  {
+    id: "lOthvD1rMbQ",
+  },
+  {
+    id: "nXDk86lQhto",
+  },
+];
+
+const sliderContainer = document.querySelector("#slider");
+const currentContainer = document.querySelector("#current");
+const videosContainer = document.querySelector("#videos-container");
+const bNext = document.querySelector(".next");
+const bPrev = document.querySelector(".prev");
+let current = 0;
+
+bShowMobileLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+  mobileMenu.classList.toggle("show");
+});
+
+moreOptions.addEventListener("click", (e) => {
+  e.preventDefault();
+  moreMenu.classList.toggle("show");
+});
+
+bNext.addEventListener("click", (e) => {
+  let changed = current;
+  current = current + 1 < videos.length ? current + 1 : current;
+  if (current !== changed) {
+    renderCurrentVideo(videos[current].id);
+  }
+});
+
+bPrev.addEventListener("click", (e) => {
+  let changed = current;
+  current = current - 1 >= 0 ? current - 1 : current;
+  if (current !== changed) {
+    renderCurrentVideo(videos[current].id);
+  }
+});
+
+renderCurrentVideo(videos[current.id]);
+//llamar nuestra galeria actual
+renderVideos();
+
+function renderCurrentVideo(id) {
+  currentContainer.innerHTML = ` <iframe width="100%" height="720" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  `;
+}
+
+//renderisar nuestros videos lista
+function renderVideos() {
+  const html = videos.map((video, index) => {
+    return ` 
+
+    <div class = "item">
+    <a  href = "#" data-id=${index}>
+    <img src="https://i3.ytimg.com/vi/${video.id}/mqdefault.jpg" />
+    </a>
+    <div>
+    `;
+  });
+
+  videosContainer.innerHTML = html.join("");
+}
